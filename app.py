3cbe292c88e6948ee8c06a38af7f7de89b0ac2a1
Rishabh Mathur <rishabhmathur106@outlook.com>
2020-09-08 21:20:50 +0530

    Optimizing the Output

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
 import json
-import difflib
-from difflib  import SequenceMatcher
+from difflib import get_close_matches
 data = json.load(open('data.json'))
 
 def translate(keyword):
     keyword = keyword.lower()
+
     if keyword in data:
         return data[keyword]
+
+    elif len(get_close_matches(keyword, data.keys(), cutoff=0.72)) > 0 :
+
+        choice = input("Did you mean %s instead? [Yes(Y)/No(N)]: " % get_close_matches(keyword, data.keys())[0])
+
+        if choice.lower() =='yes' or choice.lower() == 'y':
+            return data[get_close_matches(keyword, data.keys())[0]]
+
+        elif choice.lower() == 'no' or choice.lower() == 'n':
+            return "Sorry the word doesn't exist in my dictionary. Please check it."
+
+        else:
+            return "I didn't understand your entry"
+
     else:
         return "Sorry!! I do not have this word in my memory"
 
 input_keyword = input("Enter a word : ")
 
-print(translate(input_keyword))
+result = translate(input_keyword)
+
+if type(result) == list:
+    for item in result:
+        print(item)
+else:
+    print(result)
